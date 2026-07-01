c1b3ec9969d131287c43e012d715653184e9a7e1
jhmezzomo-hub <jmezzomo@alumno.huergo.edu.ar>
2026-07-01 18:07:06 -0300

    Ya podes desafiar por una medalla, falta hacer qeu se compruebe de que no la tenga

--- desafio.py
+++ desafio.py
@@ -1,20 +1,31 @@
 from medallas_set import cargar_medallas
+from time import sleep
 
-def desafio_gimansio(gimansio):
+def desafio_gimansio(contrincante):
     victoria = False
-    lider, medalla = gimansio
-    print(f"Has entrado al gimanasio del lider {lider} y lo has desafiado")
+    medalla = contrincante
+    print(f"Has entrado al gimanasio del lider y lo has desafiado")
     #Iria la instancia de combate
     if victoria:
         print(f"Felicidades has obtenido la {medalla}")
 
 def dasafiar_gimansio():
-    i = 1
-    medallas = cargar_medallas()
-    for bucket in medallas.buckets:
+    medallas = []
+    las_medallas = cargar_medallas()
+    for bucket in las_medallas.buckets:
         if bucket:
             for medalla in bucket:
-                print(f"{i} - {medalla}")
-                i += 1
+                medallas.append(medalla)
+
+    try:
+        for i, e in enumerate(medallas, 1):
+            print(f"{i} - {e}")
+        a_desafiar = int(input(f"Cual gimansio deseas enfrentar?\n"))
+        contrincante = las_medallas.buscar_elemento(medallas[a_desafiar-1])
+        desafio_gimansio(contrincante)
+    except Exception as e:
+        print("Por favor ingrese un numero dentro de los mostrados")
+        print(e)
+        dasafiar_gimansio()
 
 dasafiar_gimansio()
